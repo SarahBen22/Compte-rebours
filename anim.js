35d589683aa539415b3fa7ab32ae2e6e7d99c3e3
SarahBen22 <sa.tahiris@gmail.com>
2018-05-11 10:17:17 +0100

    update

--- anim.js
+++ anim.js
@@ -11,43 +11,37 @@ jQuery(function($){
   var pause=document.getElementById("stop");
 
   // compter chaque seconde
-  var t = setInterval(function(){
+  var t = setInterval(setDate,1000);
 
-  setDate();
-  function setDate(){
-    var now= new Date();
-    var s=(launch.getTime() -now.getTime())/1000; //getTime récupère le temps ici en millisecondes
-
-
-    var j=Math.floor(s/86400);
-  days.html('<strong>'+j+'</strong>Day'+ (j>1?'s':'')); // ?= if; :=else (autre façon de l'écrire)
-  s-=j*86400; //soustraction des secondes s-= équivaut s=s-...
-
-    var h=Math.floor(s/3600);
-    hours.html('<strong>'+h+'</strong>Hour'+ (h>1?'s':''));
-  s-=h*3600;
-
-    var m=Math.floor(s/60);
-  minutes.html('<strong>'+m+'</strong>Minute'+ (m>1?'s':''));
-  s-=m*60;
-
-    var s=Math.floor(s);
-  seconds.html('<strong>'+s+'</strong>Second'+ (s>1?'s':''));
-
-  // setTimeout est un timer en JS et donc là
-  setTimeout(setDate,1000);
+function setDate(){ // calcule et affiche la distance entre la date demandée et celle d'aujourd'hui
+  var now= new Date();
+  var s=(launch.getTime() -now.getTime())/1000; //getTime récupère le temps ici en millisecondes
 
 
-    console.log(now.toLocaleString());
-  }
+  var j=Math.floor(s/86400);
+days.html('<strong>'+j+'</strong>Day'+ (j>1?'s':'')); // ?= if; :=else (autre façon de l'écrire)
+s-=j*86400; //soustraction des secondes s-= équivaut s=s-...
 
-},1000);
+  var h=Math.floor(s/3600);
+  hours.html('<strong>'+h+'</strong>Hour'+ (h>1?'s':''));
+s-=h*3600;
 
+  var m=Math.floor(s/60);
+minutes.html('<strong>'+m+'</strong>Minute'+ (m>1?'s':''));
+s-=m*60;
+
+  var s=Math.floor(s);
+seconds.html('<strong>'+s+'</strong>Second'+ (s>1?'s':''));
+
+  console.log(now.toLocaleString());
+}
 
 pause.onclick = function() {
     clearInterval(t);
-  console.log("pause");
-  console.log(t);
-    }
+  } // pour que lorsque j'appuie sur bouton stop mon Compte à rebours s'arrête
+  start.onclick = function() {
+      t=setInterval(setDate,1000);
+
+    }// pour que lorsque j'appuie sur bouton play mon Compte à rebours reprend
 
 });
